3ae9bb7a7243d7ae26bfd37e08e90e392c3ba484
居戎氏 <chen.sst@gmail.com>
2025-06-04 16:09:53 +0800

    lemon horizon

--- build.rs
+++ build.rs
@@ -65,7 +65,8 @@ fn generate_vial_config() {
     let mut content = String::new();
     match File::open(p) {
         Ok(mut file) => {
-            file.read_to_string(&mut content).expect("Cannot read vial.json");
+            file.read_to_string(&mut content)
+                .expect("Cannot read vial.json");
         }
         Err(e) => println!("Cannot find vial.json {:?}: {}", p, e),
     };
